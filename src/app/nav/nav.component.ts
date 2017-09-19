dce666a1f040a0ac17864ca73f8f0b6c160556dc
Himanshu Pant <himanshu.pn3@gmail.com>
2017-09-19 01:50:55 -0700

    added search, ask question, request topic options to nav component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,14 @@
-import { Component } from "@angular/core"
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'nav-component',
-    templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.scss']
+  selector: 'app-nav-component',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
 })
-
 export class NavComponent {
-    
+    displayAskQuestion = false;
+    onAskQuestion () {
+        this.displayAskQuestion = true;
+        console.log('hey')
+    }
 }
